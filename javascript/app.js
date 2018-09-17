2f4eb80136dce5a2fb58060a9b658b0be8820660
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-17 18:46:21 -0500

    2.Data Structures - Finished

--- javascript/app.js
+++ javascript/app.js
@@ -2,16 +2,15 @@ console.log("JS IS RUNNING!");
 
 
 // 1. Data types
+
 // Refresher
 // So far we have seen:
-
 // Strings
 // Numbers
 // Booleans
 // Arrays
 // Objects
 // When these datatypes are combined, we get a data structure, for example, an array that contains objects that each contain booleans.
-
 // Suppose you are tasked with creating some software, and it is up to you to determine which datatypes and what data structure to use. For each of the following, write which data types you would use to represent the data, and then give a small example of the data structure:
 
 // • A light switch that can be either on or off.
@@ -65,12 +64,36 @@ let studentNames3 = [
 }
 ]
 
-console.log(spaceship);
-console.log(studentNames);
-console.log(studentNames2);
-console.log(studentNames3);
+console.log(spaceship); // TEST
+console.log(studentNames); // TEST
+console.log(studentNames2); // TEST
+console.log(studentNames3); // TEST
 ////////////////////////////////////////////
 // 🔴 Commit: "data types"
+
+
+
+// 2. Take it Easy
+
+// Make an array that holds all of the colors of the rainbow.
+let rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(rainbow); // TEST
+
+// Write code that will access "blue" from the rainbow array.
+console.log(rainbow[4]);
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+let cha = {
+	fave_food: "At the moment, thai curry",
+	hobby: "Reading",
+	town: "Chicago",
+	fave_datatype: "........probably Booleans"
+}
+
+// Write code that will access your hobby from the object that you just created.
+console.log(cha.hobby);
+////////////////////////////////////////////
+// 🔴 Commit: "data structures"
 
 
 
